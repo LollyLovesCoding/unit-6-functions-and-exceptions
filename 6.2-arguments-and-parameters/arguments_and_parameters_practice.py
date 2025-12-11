a0dab80125bb9d5c36adb2cd21fd288740f93f43
Lilyana Zhang <impro4everrr@gmail.com>
2025-12-11 10:26:36 -0500

    Create arguments_and_parameters_practice.py

---
+++ 6.2-arguments-and-parameters/arguments_and_parameters_practice.py
@@ -0,0 +1,33 @@
+# 1. 
+# - Two arguments are sent to the "num_chairs" function, which are "4" and "5"
+# - The parameter "tables" is assigned the value "4" and "chairs_per_table" is assigned the value "5"
+# - The output is "You will need 20 chairs."
+# - Removing or adding arguments will result in a TypeError since this function only takes 2 arguments.
+
+# 2. 
+def print_integer(int):
+    print(int)
+
+print_integer(5)
+
+# 3.
+def print_integers_difference(int1, int2):
+    print(abs(int1 - int2))
+
+print_integers_difference(4, 6)
+
+# 4.
+# 1. The keyword to define the function, "function", will not work. Instead, fix it to "def".
+# 2. The function should take 2 arguments, so there should be a second parameter "b" in the brackets.
+
+# 5. 
+def activate_thrusters(percent_power):
+    pass
+
+activate_thrusters(1)
+activate_thrusters(100)
+
+# 6. 
+# 1. The function does not have brackets, meaning it will not execute.
+# 2. There are no arguments passed into the function. There should be 2: name and apples to eat.
+# Correct way: e.g., person_apples("Lilyana", 3)
