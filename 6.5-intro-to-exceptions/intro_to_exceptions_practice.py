22c537ec6498f1f8ae61e4cbb23032c9315bf5a3
Lilyana Zhang <impro4everrr@gmail.com>
2026-01-24 12:24:06 -0500

    Create intro_to_exceptions_practice.py

---
+++ 6.5-intro-to-exceptions/intro_to_exceptions_practice.py
@@ -0,0 +1,24 @@
+# 1
+"abc" / 3 # TypeError
+
+# 2
+True + "story" # TypeError
+
+# 3
+5 / 0 # TypeError
+
+# 4
+print(name) # NameError
+
+# 5
+print "Hello") # SyntaxError
+
+# 6
+mark = int("one hundred") # ValueError
+
+# 7
+mark = int("50.79") # ValueError
+
+# 8
+print("something's missing) 
+# SyntaxError ☝️
