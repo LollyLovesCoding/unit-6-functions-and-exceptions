3482c639e3a6fc84445819865f862d9c53d38b91
Lilyana Zhang <impro4everrr@gmail.com>
2026-01-25 15:36:26 -0500

    Create try_a_validation_loop.py

---
+++ 6.6-handling-exceptions/try_a_validation_loop.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        try:
+            age = int(input("Please enter your age: "))
+            break
+        except ValueError:
+            print("Need to input an integer!\n")
+    print(f"Wow, you are {age} years old.")
+
+if __name__ == "__main__":
+    main()
